5c51824a34d7b0271444590c3c025dad0d81c86e
Julian Eduardo Rengifo Cortes <julian.rengifocor@campusucc.edu.co>
2021-09-02 20:02:54 -0500

    0.1. mi primer commit en git para el programa

---
+++ my_program.cpp
@@ -0,0 +1,22 @@
+<stdio.h>
+<math.h>
+<getch.h>
+
+#workspace 'curso devops'
+
+static int main[object ...]{
+int ivalor = 0;
+int icontador = 0;
+cin<< 'Ingrese el valor para calcular la suma';
+
+for (i = 1; i <= ivalor; i++){
+icontador+=i;
+}
+
+cout >> 'El resultado es: '+%int,icontador';
+
+getch();
+
+exit 1;
+
+}
